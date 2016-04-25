5f5a71d2a7967dcc71d24f4b320aa0cecf7f6270
Monster <monster.lixinghan@gmail.com>
2016-04-25 15:29:35 +0800

    add grammar reader

---
+++ javascript/parser.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def read_file():
+    fname = sys.path[0] + '/javascript.grm'
+    grammars = {}
+    with open(fname, 'r') as f:
+        current_nonterminal_symbolic = None
+        for line in f:
+            line = line.strip()
+            if line.endswith(':'):
+                current_nonterminal_symbolic = line[:-1].strip()
+                grammars[current_nonterminal_symbolic] = []
+            elif len(line) > 0:
+                grammars[current_nonterminal_symbolic].append(line.split())
+            else:
+                pass
+    return grammars
+
+def main(argv):
+    grammars = read_file()
+    print grammars
+
+if __name__ == '__main__':
+    main(sys.argv)
+
