6b66a4320baf4400a590bc8740057595f442ba21
Monster <monster.lixinghan@gmail.com>
2016-05-12 10:36:01 +0800

    add unittest

--- test/test.py
+++ test/test.py
@@ -1,1 +1,12 @@
-print test
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testhello(self):
+        self.assertEqual(0, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
