f8c50da0f281cbd34aea24ca3dcae9920ff7b399
Monster <monster.lixinghan@gmail.com>
2016-05-12 20:19:28 +0800

    refactor new parser with pyparsing

--- xsssb/new_parser.py
+++ xsssb/new_parser.py
@@ -22,22 +22,7 @@ class Grammar(dict):
 
 grammar = Grammar()
 
-grammar.emptyStatement = pp.Empty()
-grammar.initialiser = token.eq + grammar.assignmentExpression
-grammar.variableDeclaration = token.id + pp.Optional(grammar.initialiser)
-grammar.variableDeclarationTail = token.comma + grammar.variableDeclaration
-grammar.variableDeclarationList = grammar.variableDeclaration + pp.ZeroOrMore(grammar.variableDeclarationTail)
-grammar.variableStatement = token.k_var + grammar.variableDeclarationList + token.semi
-grammar.block = token.l_brace + grammar.statementList + token.r_brace
-grammar.statement = grammar.block ^ grammar.variableStatement ^ grammar.emptyStatement ^ grammar.expressionStatement ^ grammar.ifStatement ^ grammar.iterationStatement ^ grammar.continueStatement ^ grammar.breakStatement ^ grammar.returnStatement ^ grammar.withStatement ^ grammar.labelledStatement ^ grammar.switchStatement ^ grammar.throwStatement ^ grammar.tryStatement
-grammar.statementList = pp.OneOrMore(grammar.statement)
-grammar.formalParameterListTail = pp.Group(token.comma + token.id)
-grammar.formalParameterList = token.id + pp.ZeroOrMore(grammar.formalParameterListTail)
-grammar.functionBody = grammar.sourceElements
-grammar.functionDeclaration = token.k_function + token.id + token.l_par + pp.Optional(grammar.formalParameterList) + token.r_par + token.l_brace + grammar.functionBody + token.r_brace
-grammar.sourceElement = grammar.statement ^ grammar.functionDeclaration
-grammar.sourceElements = pp.OneOrMore(grammar.sourceElement)
-grammar.program = grammar.sourceElements + pp.StringEnd()
+grammar.PrimaryExpression = token.k_this ^ grammar.ObjectLiteral ^ pp.Group(token.l_par ^ grammar.Expression ^ token.r_par) ^ token.id ^ grammar.ArrayLiteral ^ token.Literal
 
 
 if __name__ == "__main__":
